1512f00e82873f58b5ddc06b837fc82ad3385464
Mohammed Jewel <jewelnabin@gmail.com>
2022-02-17 21:54:10 +0600

    last-part

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
+function getInputValue(id) {
+    const inputField = document.getElementById(id);
+    const inputValue = parseFloat(inputField.value);
+    // inputField.value = '';
+    return inputValue;
+
+}
+
+
+// Calculate Button Evenet Handler Add
 document.getElementById('calculator-button').addEventListener('click', function () {
-    // Calculae Food Expense //
-    const foodInput = document.getElementById('food-input');
-    const foodAmountText = foodInput.value;
-    const foodAmount = parseFloat(foodAmountText);
-    foodInput.value = '';
-    // Calculae rent Expensef //
-    const rentInput = document.getElementById('rent-input');
-    const rentAmountText = rentInput.value;
-    const rentAmount = parseFloat(rentAmountText);
-    rentInput.value = '';
-    // Calculae clothes Expense //
-    const clothesInput = document.getElementById('clothes-input');
-    const clothesAmountText = clothesInput.value;
-    const clothesAmount = parseFloat(clothesAmountText);
-    clothesInput.value = '';
-    // Total Expense //
-    const totalExpense = document.getElementById('total-expenses');
-    const totalExpenseAmount = foodAmount + rentAmount + clothesAmount;
-    totalExpense.innerText = totalExpenseAmount;
-    // Total Income //
-    const incomeInput = document.getElementById('income-input');
-    const incomeAmountText = incomeInput.value;
-    const incomeAmount = parseFloat(incomeAmountText);
-    incomeInput.value = '';
-    // Total Balance //
-    const totalIncome = document.getElementById('total-balance');
-    const totalIncomeAmount = incomeAmount;
-    totalIncome.innerText = totalIncomeAmount - totalExpenseAmount;
-    const netBalance = totalIncome - totalExpense;
+    // Get income field value and parse to float
+    const income = getInputValue('income-input');
 
-    // Balance //
 
-});
+    // Get Food, rent, cloths field value & parse to float
+    const foodCost = getInputValue('food-input');
+    const rentCost = getInputValue('rent-input');;
+    const clothCost = getInputValue('clothes-input');
+    const totalCost = foodCost + rentCost + clothCost;
+
+    if (foodCost >= 0 && rentCost >= 0 && clothCost >= 0 && income > totalCost) {
+
+        const getTotalCost = document.getElementById('total-expenses');
+        getTotalCost.innerText = totalCost;
+
+        // Set Balace
+        const getCurrentBalance = document.getElementById('total-balance');
+        getCurrentBalance.innerText = income - totalCost;
+    }
+    else if (totalCost > income) {
+        alert('Sorry! Income can\'t\ be less than expenses.');
+    }
+    else {
+        alert('Sorry! Please Input a positive number');
+    }
+
+
+})
 
 document.getElementById('save-handler').addEventListener('click', function () {
-   // Calculate Percentage //
-    const saveInput = document.getElementById('save-input');
-    const saveAmountText = saveInput.value;
-    const saveAmount = parseFloat(saveAmountText);
-    saveInput.value = '';
+    const saving = getInputValue('save-input');
+    const income = getInputValue('income-input');
 
-    // Saving Balance //
-    const totalSave = document.getElementById('saving-amount');
-    const totalSaveAmount = saveAmount;
-    totalSave.innerText = totalIncomeAmount / 20;
-    // Remaining Balance //
-    const totalRemaining = document.getElementById('remaining-balance');
-    const totalRemainingAmount = saveAmount;
-    totalIncome.innerText = totalIncomeAmount - totalExpenseAmount;
-    const netBalance = netBalance - saveAmount;
-});
+
+    const getsavingField = document.getElementById('saving-amount');
+    const savingAmount = (income / 100) * saving;
+
+    const currenBalanceText = document.getElementById('total-balance').innerText;
+    const currentBalance = parseFloat(currenBalanceText);
+
+    if (saving > 0 && savingAmount < currentBalance) {
+        getsavingField.innerText = savingAmount;
+
+        const remainingBalance = currentBalance - savingAmount;
+        document.getElementById('remaining-balance').innerText = remainingBalance;
+    }
+    else if (saving < 0) {
+        alert('Please Input a positive Number');
+    }
+
+    else {
+        alert('Sry! Your Savings can\'t\ be greater than your current balance');
+    }
+
+})
